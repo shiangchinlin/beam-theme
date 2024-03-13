4622ad5e679bf4b08b31837ca957e4277c9fd2e9
shiangchinlin <150076701+shiangchinlin@users.noreply.github.com>
2024-03-13 15:54:28 -0700

    wrok archive

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,6 +10,9 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+	<?php beam_theme_post_thumbnail(); ?>
+
 	<header class="entry-header">
 		<?php
 		if ( is_singular() ) :
@@ -29,24 +32,22 @@
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php beam_theme_post_thumbnail(); ?>
-
 	<div class="entry-content">
 		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'beam-theme' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+		// the_content(
+		// 	sprintf(
+		// 		wp_kses(
+		// 			/* translators: %s: Name of current post. Only visible to screen readers */
+		// 			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'beam-theme' ),
+		// 			array(
+		// 				'span' => array(
+		// 					'class' => array(),
+		// 				),
+		// 			)
+		// 		),
+		// 		wp_kses_post( get_the_title() )
+		// 	)
+		// );
 
 		wp_link_pages(
 			array(
@@ -57,7 +58,4 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php beam_theme_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
