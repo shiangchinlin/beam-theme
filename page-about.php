02c42e2f9a538746ab8ab83c231ce718b9223ae6
shiangchinlin <150076701+shiangchinlin@users.noreply.github.com>
2024-03-14 13:12:57 -0700

    updates about page acf

--- page-about.php
+++ page-about.php
@@ -17,20 +17,44 @@ get_header();
 
 	<main id="primary" class="site-main">
 
+
+		<div class="entry-content">
+			<?php 
+			if ( function_exists ( 'get_field' ) ) {
+		
+				if ( get_field( 'about_heading1' ) ) {
+					echo '<h2>'. get_field( 'about_heading1' ) .'</h2>';
+				}
+				
+				if ( get_field( 'about_image' ) ) {
+					echo wp_get_attachment_image( get_field( 'about_image' ), 'medium', '', array( 'class' => 'aligncenter' ));
+				}
+
+				if ( get_field( 'about_text1' ) ) {
+					the_field( 'about_text1' );
+				}
+
+				if ( get_field( 'about_heading2' ) ) {
+					echo '<h2>'. get_field( 'about_heading2' ) .'</h2>';
+				}
+
+				if ( get_field( 'about_text2' ) ) {
+					the_field( 'about_text2' );
+				}
+		
+			} 
+			?>
+		</div>
+		
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
 			get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
 		endwhile; // End of the loop.
 		?>
-
+		
 	</main><!-- #main -->
 
 <?php
